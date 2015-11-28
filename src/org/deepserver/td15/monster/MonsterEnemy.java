42dd8f57b457666384cc3c1a9e23789d3c2ed779
DonRichie <richardclauss@ricl.de>
2015-11-28 19:09:51 +0100

    A new enemy is born

---
+++ src/org/deepserver/td15/monster/MonsterEnemy.java
@@ -0,0 +1,12 @@
+package org.deepserver.td15.monster;
+
+import org.deepserver.td15.World;
+
+public class MonsterEnemy extends Monster{
+
+	public MonsterEnemy(World world) {
+		super(world);
+
+	}
+	
+}
