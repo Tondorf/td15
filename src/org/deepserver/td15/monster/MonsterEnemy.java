bb9540017ec96811fb13c48dd36fc87268d9dbc5
DonRichie <richardclauss@ricl.de>
2015-11-28 20:43:05 +0100

    Neuer Name

--- src/org/deepserver/td15/monster/MonsterEnemy.java
+++ src/org/deepserver/td15/monster/MonsterEnemy.java
@@ -6,10 +6,10 @@ import org.joml.Matrix2;
 import org.joml.Vec2;
 
 public class MonsterEnemy extends MonsterSprite {
-	protected final float maxSpeed = 0.1f;
+	protected final float maxSpeed = 2.0f;
 	protected final float rotationSpeed = 0.5f;
 	protected final int refreshTargetDelay = 1000;
-	protected final float accel = 0.01f;
+	protected final float accel = 0.1f;
 
 	protected boolean turnLeft = false;
 	protected boolean turnRight = false;
@@ -19,7 +19,7 @@ public class MonsterEnemy extends MonsterSprite {
 	protected Vec2 position;
 	protected Vec2 target;
 	protected boolean reachedMaxSpeed = false;
-	protected float v = 1;
+	protected float v = 0.0f;
 
 	public MonsterEnemy(World world, Vec2 position) {
 		super(world, position);
@@ -32,7 +32,7 @@ public class MonsterEnemy extends MonsterSprite {
 		return "ship1.png";
 	}
 
-	protected void targetPlayer(Vec2 target) {
+	protected void lockTarget(Vec2 target) {
 		this.target = target;
 
 		turnRight = true;
@@ -46,7 +46,7 @@ public class MonsterEnemy extends MonsterSprite {
 		long now = System.currentTimeMillis();
 		if ((lastTargetRefresh + refreshTargetDelay) < now) {
 			lastTargetRefresh = now;
-			targetPlayer(new Vec2(10f,10f));
+			lockTarget(new Vec2(10f,10f));
 		}
 
 		if (turnLeft) {
@@ -65,6 +65,8 @@ public class MonsterEnemy extends MonsterSprite {
 
 		Vec2 temp = new Vec2(orientation.getAhead());
 		temp.mul(v);
+		
+		System.out.println(v);
 
 		position.add(temp);
 	}
