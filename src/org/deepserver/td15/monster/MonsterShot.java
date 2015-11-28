0365fd1d48a47425f6ecd0ac73ac0fde52b7e310
DonRichie <richardclauss@ricl.de>
2015-11-28 14:47:39 +0100

    Bugfix

--- src/org/deepserver/td15/monster/MonsterShot.java
+++ src/org/deepserver/td15/monster/MonsterShot.java
@@ -1,12 +1,12 @@
 package org.deepserver.td15.monster;
 
 import org.deepserver.td15.World;
+import org.joml.Vec3;
 
 public class MonsterShot extends Monster {
 	protected final float speed = 10.0f;
 
 	public MonsterShot(World world, Vec3 position) {
 		super(world);
-		groundDistance = 1f;
 	}
 }
