695549c18f42daa7f2794fa9c8f128cbb1d0c48e
DonRichie <richardclauss@ricl.de>
2015-11-28 21:23:10 +0100

    enemy knows player

--- src/org/deepserver/td15/screen/ScreenStartGame.java
+++ src/org/deepserver/td15/screen/ScreenStartGame.java
@@ -20,7 +20,7 @@ public class ScreenStartGame extends Screen {
 		player.position = new Vec2(0f, 0f);
 		world.add(player);
 
-		MonsterEnemy enemy = new MonsterEnemy(world, new Vec2(1f,1f));
+		MonsterEnemy enemy = new MonsterEnemy(world, new Vec2(1f,1f), player);
 		world.add(enemy);
 
 		for (int i = 0; i < monsterCount; i++) {
