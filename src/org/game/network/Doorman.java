3dcf8a025155c728b2b09d858c9550a28848d051
Nis Meinert <mail@nismeinert.de>
2015-11-28 10:57:30 +0100

    added log4j to project

--- src/org/game/network/Doorman.java
+++ src/org/game/network/Doorman.java
@@ -3,11 +3,15 @@ package org.game.network;
 import java.io.IOException;
 import java.net.Socket;
 
+import org.apache.log4j.Logger;
+
 /*
  * Doorman waits for clients to connect.
  * He accepts them and passes them to the server for registration.
  */
 public class Doorman extends Thread {
+	private static Logger logger = Logger.getLogger(Doorman.class);
+	
 	private Server server;
 	
 	public Doorman(Server server) {
@@ -16,10 +20,13 @@ public class Doorman extends Thread {
 
 	@Override
 	public void run() {
+		logger.info("Waiting for new clients...");
 		while (true) {
 			try {
 				Socket clientSocket = server.getServerSocket().accept();
 				server.registerNewClient(clientSocket);
+				
+				logger.info("Accepted new client");
 			} catch (IOException e) {
 				System.err.println(e.getMessage());
 			} catch (AboveCapacityException e) {
