a4379166c8ce75657bde7af20d5946bc4b2deb1f
tkarrass <tkarrass@bitsetter.de>
2015-11-28 11:24:00 +0100

    Use Floathing for menu shading

--- src/org/deepserver/td15/Floathing.java
+++ src/org/deepserver/td15/Floathing.java
@@ -1,48 +1,58 @@
 package org.deepserver.td15;
 
 public class Floathing {
-	protected float goal=0;
-	protected float speed=0;
-	
-	protected float current=0;
-	
-	public Floathing(float goal,float speed) {
-		this.goal=goal;
-		this.speed=speed;
+	protected float goal = 0;
+	protected float speed = 0;
+
+	protected float current = 0;
+
+	public Floathing(float current, float goal, float speed) {
+		this.current = current;
+		this.goal = goal;
+		this.speed = speed;
 	}
-	
+
+	public Floathing(float goal, float speed) {
+		this.goal = goal;
+		this.speed = speed;
+	}
+
 	public Floathing(float speed) {
-		this.goal=0;
-		this.speed=speed;
+		this.goal = 0;
+		this.speed = speed;
 	}
-	
+
 	public float update(double delta, boolean up, boolean down) {
-		float aimFor=0;
+		float aimFor = 0;
 
-		if (up) aimFor=goal;
-		if (down) aimFor=-goal;
-		
-		return update(delta,aimFor);
+		if (up)
+			aimFor = goal;
+		if (down)
+			aimFor = -goal;
+
+		return update(delta, aimFor);
 	}
-	
-	public float update(double delta, float aimFor) {		
-		float max=speed*(float)delta;
 
-		if (current<aimFor) {
-			float offset=aimFor-current;
-			if (offset>max) offset=max;
-			current+=offset;
+	public float update(double delta, float aimFor) {
+		float max = speed * (float) delta;
+
+		if (current < aimFor) {
+			float offset = aimFor - current;
+			if (offset > max)
+				offset = max;
+			current += offset;
 		}
-		
-		if (current>aimFor) {
-			float offset=current-aimFor;
-			if (offset>max) offset=max;
-			current-=offset;
+
+		if (current > aimFor) {
+			float offset = current - aimFor;
+			if (offset > max)
+				offset = max;
+			current -= offset;
 		}
-		
+
 		return current;
 	}
-	
+
 	public float get() {
 		return current;
 	}
